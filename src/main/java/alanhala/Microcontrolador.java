77061c2897a3ba8941b37853b0ccf99666045024
Alan Halatian <alanhalatian@hotmail.com>
2015-07-19 14:04:46 -0300

    agregue metodos de estado y funcion de deshacer

--- src/main/java/alanhala/Microcontrolador.java
+++ src/main/java/alanhala/Microcontrolador.java
@@ -3,15 +3,64 @@ package alanhala;
 import java.util.ArrayList;
 import java.util.List;
 
+import alanhala.estados.Detenido;
+import alanhala.estados.Estado;
+import alanhala.instrucciones.Instruccion;
+
 public class Microcontrolador {
 	private int a;
 	private int b;
 	private int[] memoria;
+	private List<Instruccion> programa;
+	private Estado estado;
+	private int counter;
+	private Microcontrolador estadoAnterior;
 
 	public Microcontrolador() {
 		a = 0;
 		b = 0;
 		memoria = new int[1024];
+		programa = new ArrayList<Instruccion>();
+		estado = new Detenido();
+		counter = 0;
+	}
+
+	public int[] getMemoria() {
+		return memoria;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+	public Microcontrolador getEstadoAnterior() {
+		return estadoAnterior;
+	}
+
+	public Microcontrolador(int a, int b, int[] memoria,
+			List<Instruccion> programa, Estado estado, int counter) {
+		this.a = a;
+		this.b = b;
+		this.memoria = memoria;
+		this.programa = programa;
+		this.estado = estado;
+		this.counter = counter;
+	}
+
+	public void ejecutarInstruccion(Instruccion instruccion) {
+		estadoAnterior = this.clonar();
+		instruccion.ejecutar(this);
+	}
+
+	public Microcontrolador clonar() { // era una chota usar el metodo clone.
+										// tenia que agregar una excepcion cada
+										// vez que lo usaba. este metodo tambien
+										// me parece choto.
+		return new Microcontrolador(a, b, memoria, programa, estado, counter);
 	}
 
 	public void distribuirResultado(int resultado) {
@@ -42,7 +91,7 @@ public class Microcontrolador {
 
 	}
 
-	public int getMemoria(int address) {
+	public int getInfoDeMemoria(int address) {
 		return memoria[address];
 	}
 
@@ -50,4 +99,66 @@ public class Microcontrolador {
 		memoria[address] = a;
 	}
 
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public List<Instruccion> getPrograma() {
+		return programa;
+	}
+
+	public void load(List<Instruccion> programa) {
+		estado.load(programa);
+		this.programa = programa;
+		counter = 0;
+	}
+
+	public void start() {
+		estado.start(this);
+		memoria = new int[1024];
+	}
+
+	public void execute() {
+		estado.execute();
+		this.ejecutarSiguienteInstruccion();
+	}
+
+	public void ejecutarSiguienteInstruccion() {
+		this.ejecutarInstruccion(programa.get(counter));
+		counter++;
+		if (this.finDePrograma()) {
+			this.stop();
+		}
+	}
+
+	public boolean finDePrograma() {
+		return counter == programa.size();
+	}
+
+	public void stop() {
+		estado.stop(this);
+		counter = 0;
+	}
+
+	public void ejecutarPrograma() { // aca no se si verificar que el micro este
+										// detenido o en ejecucion
+		// En caso de hacerlo podria usar
+		// micro.start();
+		if (programa.isEmpty())
+			throw new RuntimeException("Tiene que haber un programa cargado");
+		else
+			programa.forEach(instruccion -> this
+					.ejecutarInstruccion(instruccion));
+	}
+
+	public void deshacer() { // me parece demasiado choto este metodo. Aumentan
+								// los atributos y tengo que estar agregandolos
+								// todo el tiempo
+		a = estadoAnterior.getA();
+		b = estadoAnterior.getB();
+		memoria = estadoAnterior.getMemoria();
+		programa = estadoAnterior.getPrograma();
+		estado = estadoAnterior.getEstado();
+		counter = estadoAnterior.getCounter();
+	}
 }
